5397d91743baad4971d523c7b54ade849f65e5dd
nemsi85 <valentina.carrirolo@gmail.com>
2021-03-23 00:18:02 +0100

    Clean code

--- app/src/main/java/com/github/valecarrirolo/pizzaup/Utils.kt
+++ app/src/main/java/com/github/valecarrirolo/pizzaup/Utils.kt
@@ -4,13 +4,10 @@ import android.content.Context
 import android.util.TypedValue
 import android.view.View
 
-fun formatPrice(price: Double): String {
-    return if (price.toInt().toDouble() == price) {
-        "${price.toInt()}€"
-    } else {
-        "${price}€"
-    }
-}
+fun formatPrice(price: Double) =
+    if (price.toInt().toDouble() == price) "${price.toInt()}€" else "${price}€"
 
-fun Context.toPixelFromDip(value: Float) = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, resources.displayMetrics)
+fun Context.toPixelFromDip(value: Float) =
+    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, resources.displayMetrics)
+
 fun View.toPixelFromDip(value: Float) = context.toPixelFromDip(value)
