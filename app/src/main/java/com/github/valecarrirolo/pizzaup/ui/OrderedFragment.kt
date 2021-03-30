0805d0a1443c38394a7ad062cdb0312ca38b44f8
nemsi85 <valentina.carrirolo@gmail.com>
2021-03-31 01:05:26 +0200

    Use .addRepeatingJob() on flow

--- app/src/main/java/com/github/valecarrirolo/pizzaup/ui/OrderedFragment.kt
+++ app/src/main/java/com/github/valecarrirolo/pizzaup/ui/OrderedFragment.kt
@@ -6,11 +6,14 @@ import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.addRepeatingJob
 import com.github.valecarrirolo.pizzaup.MainAdapter
 import com.github.valecarrirolo.pizzaup.MainViewModel
 import com.github.valecarrirolo.pizzaup.databinding.FragmentOrderedBinding
 import com.github.valecarrirolo.pizzaup.formatPrice
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 
 class OrderedFragment : Fragment() {
     val viewModel by activityViewModels<MainViewModel>()
@@ -23,29 +26,38 @@ class OrderedFragment : Fragment() {
         .also { binding ->
 
             // Progress Bar Loading
-            viewModel.isLoading.asLiveData().observe(viewLifecycleOwner) { isLoading: Boolean ->
+            viewModel.isLoading.observe { isLoading: Boolean ->
                 binding.progressLoader.isVisible = isLoading // == true
             }
 
             // Message No Pizza Selected / Empty list
-            viewModel.isPizzasEmpty.asLiveData()
-                .observe(viewLifecycleOwner) { isPizzasEmpty: Boolean ->
-                    binding.noneSelected.isVisible = isPizzasEmpty
-                    binding.totalCost.isVisible = !isPizzasEmpty
-                }
+            viewModel.isPizzasEmpty.observe { isPizzasEmpty: Boolean ->
+                binding.noneSelected.isVisible = isPizzasEmpty
+                binding.totalCost.isVisible = !isPizzasEmpty
+            }
 
             // RecyclerView List
             val adapter = MainAdapter(viewModel)
             binding.recyclerViewExample.adapter = adapter
-            viewModel.orderedPizzas.asLiveData().observe(viewLifecycleOwner) { pizzaList ->
+            viewModel.orderedPizzas.observe { pizzaList ->
                 adapter.dataSet = pizzaList
                 adapter.notifyDataSetChanged()
             }
 
             //Total Cost ordered pizzas
-            viewModel.totalCost.asLiveData().observe(viewLifecycleOwner) {
+            viewModel.totalCost.observe {
                 binding.totalCost.text = "TOTALE: " + it.formatPrice()
             }
         }
         .root
+
+    // <T> replaces the boolean to make the function universal
+    private fun <T> Flow<T>.observe(
+        state: Lifecycle.State = Lifecycle.State.STARTED,
+        action: suspend (T) -> Unit
+    ) {
+        viewLifecycleOwner.addRepeatingJob(state) {
+            collect(action)
+        }
+    }
 }
--- app/src/main/java/com/github/valecarrirolo/pizzaup/ui/PizzaListFragment.kt
+++ app/src/main/java/com/github/valecarrirolo/pizzaup/ui/PizzaListFragment.kt
@@ -6,10 +6,14 @@ import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.addRepeatingJob
 import com.github.valecarrirolo.pizzaup.MainAdapter
 import com.github.valecarrirolo.pizzaup.MainViewModel
 import com.github.valecarrirolo.pizzaup.databinding.FragmentPizzalistBinding
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
 
 class PizzaListFragment : Fragment() {
     val viewModel by activityViewModels<MainViewModel>()
@@ -21,17 +25,27 @@ class PizzaListFragment : Fragment() {
     ) = FragmentPizzalistBinding.inflate(inflater)
         .also { binding ->
             // Progress Bar Loading
-            viewModel.isLoading.asLiveData().observe(viewLifecycleOwner) { isLoading: Boolean ->
+            viewModel.isLoading.observe { isLoading: Boolean ->
                 binding.progressLoader.isVisible = isLoading // == true
             }
 
             // RecyclerView List
             val adapter = MainAdapter(viewModel)
             binding.recyclerViewExample.adapter = adapter
-            viewModel.allPizzas.asLiveData().observe(viewLifecycleOwner) { pizzaList ->
+            viewModel.allPizzas.observe { pizzaList ->
                 adapter.dataSet = pizzaList
                 adapter.notifyDataSetChanged()
+
             }
+        }.root
+
+    // <T> replaces the boolean to make the function universal
+    private fun <T> Flow<T>.observe(
+        state: Lifecycle.State = Lifecycle.State.STARTED,
+        action: suspend (T) -> Unit
+    ) {
+        viewLifecycleOwner.addRepeatingJob(state) {
+            collect(action)
         }
-        .root
+    }
 }
